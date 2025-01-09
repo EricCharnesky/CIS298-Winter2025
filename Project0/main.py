342ee0c77b7283b056336d54c030bd757bf4ba65
Eric Charnesky <EricCharnesky@gmail.com>
2025-01-09 10:35:40 -0500

    project 0

---
+++ Project0/main.py
@@ -0,0 +1,4 @@
+print("Eric Charnesky, Doctoral Drop Out")
+print("Play D&D, Run, Read, Teach at OCC and Walsh, play with my 7 kids")
+print('Eric likes to run ( run/walk ) marathons')
+print("personalized healthcare with AI")
